8d20d0297e61102516c704deeb8b01eae6a03ae4
Yann Birba <birbayann@gmail.com>
2022-10-15 20:25:08 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/** @type {import('vite').UserConfig} */
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 import { defineConfig } from "vite";
@@ -11,21 +12,22 @@ export default defineConfig({
     }),
   ],
   build: {
+    ssr: true,
     sourcemap: true,
     minify: true,
     // cssCodeSplit: true,
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
       name: "react-first-package",
-      formats: ["es", "cjs"],
+      formats: ["es", "umd", "cjs"],
       fileName: (format) => `rfp.${format}.js`,
     },
     rollupOptions: {
       external: ["react", "react-dom"],
       output: {
-        preserveModules: true,
-        preserveModulesRoot: "src",
-        inlineDynamicImports: false,
+        // preserveModules: true,
+        // preserveModulesRoot: "src",
+        // inlineDynamicImports: false,
         globals: {
           react: "React",
           "react-dom": "ReactDOM",
